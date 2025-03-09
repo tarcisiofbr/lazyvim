20dbc85e74da49ce3394cfebe01bf8c30051fd3e
Tarcisio Brito <tarcisiofbr@gmail.com>
2025-03-08 22:31:40 -0300

    First lazy setup

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,47 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+
+-- General
+vim.keymap.set("n", "<leader>'", "<C-^>")
+vim.keymap.set("n", "<C-j>", "5<C-e>")
+vim.keymap.set("n", "<C-k>", "5<C-y>")
+vim.keymap.set("n", "<leader>m", ":split<CR>:terminal<cr>")
+vim.keymap.set("n", "<leader>z", ":w<CR>:source ~/.config/nvim/init.lua<CR>")
+
+-- Code section
+vim.keymap.set("n", "<leader>ai", "i#! <ESC>80i-<ESC>0lli  <ESC>i")
+vim.keymap.set("n", "<leader>ac", "80|C<ESC>0")
+vim.keymap.set(
+  "n",
+  "<leader>ap",
+  ":lua vim.fn.expand('%')<CR>o<C-r>%<ESC><C-v>0d:split<CR>:terminal<CR>igrep '^#!' <C-\\><C-n>pi<CR>exit"
+)
+
+-- Execute codes in interactive mode
+vim.keymap.set("n", "<leader>rl", "yy<C-w>wpi<CR><C-\\><C-n><C-w>wj")
+vim.keymap.set("n", "<leader>rr", "yy<C-w>wpi<CR><C-\\><C-n><C-w>w")
+vim.keymap.set("n", "<leader>rh", "viwy<C-w>whead(pi)<CR><C-\\><C-n><C-w>w")
+vim.keymap.set("n", "<leader>rk", "V}y<C-w>wpi<CR><C-\\><C-n><C-w>w}")
+vim.keymap.set("n", "<leader>ri", "{V}y<C-w>wpi<CR><C-\\><C-n><C-w>w}")
+vim.keymap.set("v", "<leader>rb", "y<C-w>wpi<CR><C-\\><C-n><C-w>w")
+--
+
+-- R interactive
+vim.keymap.set("n", "<leader>ro", ":split<CR>:terminal<CR>iconda activate bioinfo<CR>R<CR><C-\\><C-n><C-w>w")
+vim.keymap.set("n", "<leader>rv", ":vsplit<CR>:terminal<CR>iconda activate bioinfo<CR>R<CR><C-\\><C-n><C-w>w")
+vim.keymap.set("n", "<leader>rq", '<C-w>wiq(save="no")<CR>exit<CR>')
+vim.keymap.set("i", "<C-]>", "<Esc>a %>% ")
+vim.keymap.set("i", "<C-x>", "<Esc>a %in% ")
+vim.keymap.set("i", "<C-_>", "<Esc>a <- ")
+--
+
+-- Python interactive
+vim.keymap.set(
+  "n",
+  "<leader>pv",
+  ":vsplit<CR>:terminal<CR>iconda activate bioinfo<CR>ipython --no-autoindent<CR><C-\\><C-n><C-w>w"
+)
+vim.keymap.set("n", "<leader>pq", "<C-w>wiexit()<CR><C-\\><C-n><C-w>w<C-w>wiexit<CR>")
+-- vim.keymap.set("n", "<leader>ph", 'viwy<C-w>wpi.head()<CR><C-w>w')
---
+++ lua/config/lazy.lua
@@ -0,0 +1,107 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+vim.keymap.set({ "i" }, "<C-K>", function()
+  ls.expand()
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-L>", function()
+  ls.jump(1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
+  ls.jump(-1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-E>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, { silent = true })
+
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local extras = require("luasnip.extras")
+local l = extras.lambda
+local rep = extras.rep
+local p = extras.partial
+local m = extras.match
+local n = extras.nonempty
+local dl = extras.dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local conds = require("luasnip.extras.expand_conditions")
+local postfix = require("luasnip.extras.postfix").postfix
+local types = require("luasnip.util.types")
+local parse = require("luasnip.util.parser").parse_snippet
+local ms = ls.multi_snippet
+local k = require("luasnip.nodes.key_indexer").new_key
+--
+ls.add_snippets("all", {
+  s("section", {
+    t("#! "),
+    i(1),
+    t(" --------------------------------------------------------------------------------------"),
+  }),
+})
+--
+--
+--
